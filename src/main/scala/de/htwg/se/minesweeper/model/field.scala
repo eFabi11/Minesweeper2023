1718bd88bf63dc3e73200850723464a7e75bc5fc
Fabian EItel <fabian.eitel@yahoo.de>
2023-10-12 20:44:44 +0200

    gitignor

--- src/main/scala/de/htwg/se/minesweeper/model/field.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/field.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.minesweeper.model
 
 
-case class Field(cellNum: Int):
+case class Field(cellNum: Int){
     val endl = sys.props("line.separator")
     // defines the bar
     def bar(cellWidth: Int = 3, cellNum: Int = 3) = (("+" + "-" * cellWidth) * cellNum) + "+" + endl
@@ -10,4 +10,4 @@ case class Field(cellNum: Int):
     // defines the grid and default size is 10x10 field
     def mesh(cellWidth: Int = 3, cellNum: Int = 10) = (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
     // prints grid default size is 10x10
-    println(mesh())
+    println(mesh())}
