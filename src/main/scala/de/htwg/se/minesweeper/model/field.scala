ebc7677b87d44b1d13488a7410b83f31eb98fd6f
Fabian EItel <fabian.eitel@yahoo.de>
2023-10-20 12:38:49 +0200

    test merge conflict

--- src/main/scala/de/htwg/se/minesweeper/model/field.scala
+++ src/main/scala/de/htwg/se/minesweeper/model/field.scala
@@ -1,12 +1,12 @@
 package de.htwg.se.minesweeper.model
 
-
+//test
 case class Field(cellNum: Int){
     val endl = sys.props("line.separator")
     // defines the bar
-    def bar(cellWidth: Int = 3, cellNum: Int = 3) = (("+" + "-" * cellWidth) * cellNum) + "+" + endl
+    def bar(cellWidth: Int = 5, cellNum: Int = 5) = (("+" + "-" * cellWidth) * cellNum) + "+" + endl
     // defines the cells
-    def cells(cellWidth: Int = 3, cellNum: Int = 3) = ("|" + " " * cellWidth) * cellNum + "|" + endl
+    def cells(cellWidth: Int = 5, cellNum: Int = 5) = ("|" + " " * cellWidth) * cellNum + "|" + endl
     // defines the grid and default size is 10x10 field
     def mesh(cellWidth: Int = 3, cellNum: Int = cellNum) = (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(cellWidth, cellNum)
     // prints grid default size is 10x10
